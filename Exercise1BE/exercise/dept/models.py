ee704095ab69ae1cb26696f6c51a3d2892559c3a
AnnaThunn <ngwesinthunn1@gmail.com>
2026-05-14 20:08:59 +0630

    dep

--- Exercise1BE/exercise/dept/models.py
+++ Exercise1BE/exercise/dept/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    dept_code = models.CharField(max_length=50, unique=True)
+    dept_name = models.CharField(max_length=255)
+    status = models.BooleanField(default=True) 
+
+    def __str__(self):
+        return self.dept_name
